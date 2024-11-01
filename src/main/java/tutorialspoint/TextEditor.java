73d085465b908fd8934ce076fd3b60441bc74623
Jusun Park <jusun.park@mytrum.com>
2024-11-01 18:18:16 +0900

    Component Programming 4Step - Annotation 기반 설정 1. Spring 설정 파일에 'context' namespace 설정된 소스 2. Annotation의 예제 3. @Autowired 4. @Autowired 예제 5. @Qualifier 6. @Resource 7. @Required

---
+++ src/main/java/tutorialspoint/TextEditor.java
@@ -0,0 +1,1 @@
+package tutorialspoint;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Component;@Componentpublic class TextEditor {    @Autowired    private SpellChecker spellChecker;    public void spellCheck() {        System.out.println("Inside TextEditor constructor");    }}
